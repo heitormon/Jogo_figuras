dbbc4a4bf1efb2b49c165316b85e06f924f64d83
Resource <heitor_nobu@hotmail.com>
2019-04-26 16:00:50 -0300

    OI

--- app/src/main/java/br/com/heitor/jogo_figuras/FigurasView.java
+++ app/src/main/java/br/com/heitor/jogo_figuras/FigurasView.java
@@ -9,6 +9,8 @@ import android.graphics.Point;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,7 +18,8 @@ import java.util.List;
 import java.util.Random;
 
 public class FigurasView extends View {
-
+    private boolean telaCriada=false;
+    private ArrayList<Point> ponteiro = new ArrayList<>();
     public FigurasView(Context context){
         super(context);
     }
@@ -24,8 +27,38 @@ public class FigurasView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+            Paint paint = new Paint();
+            if(telaCriada==false) {
+                ponteiro = gerarPosições();
+                telaCriada = true;
+            }
+            gerarFiguras(canvas, paint, ponteiro);
+
+    }
+
+
+
+    public ArrayList<Point> gerarPosições(){
         ArrayList<Point> Ponteiros = new ArrayList<>();
-        Paint paint = new Paint();
+        int x = getWidth();
+        int y = getHeight();
+        Point posição1 = new Point(x/4,2*y/4);
+        Point posição2 = new Point(x/4,3*y/4);
+        Point posição3 = new Point(3*x/4,2*y/4);
+        Point posição4 = new Point(3*x/4,3*y/4);
+        Ponteiros.add(posição1);
+        Ponteiros.add(posição2);
+        Ponteiros.add(posição3);
+        Ponteiros.add(posição4);
+
+        Collections.shuffle(Ponteiros);
+
+        return Ponteiros;
+
+    }
+
+    public void gerarFiguras(Canvas canvas, Paint paint,ArrayList<Point> Ponteiros) {
+
         Path path = new Path();
         int x = getWidth();
         int y = getHeight();
@@ -35,9 +68,6 @@ public class FigurasView extends View {
         int r = random.nextInt(156)+100;
         int g = random.nextInt(156)+100;
         int b = random.nextInt(156)+100;
-
-        Ponteiros = gerarPosições();
-
 
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.WHITE);
@@ -50,7 +80,7 @@ public class FigurasView extends View {
         canvas.drawRect((Ponteiros.get(1).x)-raio,(Ponteiros.get(1).y)-raio,raio+Ponteiros.get(1).x,raio+Ponteiros.get(1).y,paint);
 
         //Triangulo
-       // canvas.drawCircle(3*x/4,2*y/4,raio,paint);
+        // canvas.drawCircle(3*x/4,2*y/4,raio,paint);
         Point a1 = new Point(Ponteiros.get(2).x, (Ponteiros.get(2).y)-raio);
         Point b2 = new Point(raio+Ponteiros.get(2).x, raio+Ponteiros.get(2).y);
         Point c3 = new Point((Ponteiros.get(2).x)-raio, raio+Ponteiros.get(2).y);
@@ -67,34 +97,7 @@ public class FigurasView extends View {
         //Retangulo
 
         canvas.drawRect((Ponteiros.get(3).x)-raio/2,(Ponteiros.get(3).y)-raio,raio/2+Ponteiros.get(3).x,raio+Ponteiros.get(3).y,paint);
-
     }
 
 
-
-
-    public boolean isCirculo(MotionEvent event, RectF bounds, int raio){
-        return true;
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
-    }
-
-    public ArrayList<Point> gerarPosições(){
-        ArrayList<Point> Ponteiros = new ArrayList<>();
-        int x = getWidth();
-        int y = getHeight();
-        Point posição1 = new Point(x/4,2*y/4);
-        Point posição2 = new Point(x/4,3*y/4);
-        Point posição3 = new Point(3*x/4,2*y/4);
-        Point posição4 = new Point(3*x/4,3*y/4);
-        Ponteiros.add(posição1);
-        Ponteiros.add(posição2);
-        Ponteiros.add(posição3);
-        Ponteiros.add(posição4);
-        Collections.shuffle(Ponteiros);
-        return Ponteiros;
-
-    }
 }
--- app/src/main/java/br/com/heitor/jogo_figuras/MainActivity.java
+++ app/src/main/java/br/com/heitor/jogo_figuras/MainActivity.java
@@ -1,16 +1,25 @@
 package br.com.heitor.jogo_figuras;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textViewF;
+    View figurasView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new FigurasView(this));
+
 
     }
+
+
 }
