2795afaee7d080cc7beb7c93a247bb1a81cbde66
Resource <heitor_nobu@hotmail.com>
2019-05-04 11:03:25 -0300

    OI

--- app/src/main/java/br/com/heitor/jogo_figuras/FigurasView.java
+++ app/src/main/java/br/com/heitor/jogo_figuras/FigurasView.java
@@ -26,45 +26,38 @@ public class FigurasView extends View {
         super(context);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-            Paint paint = new Paint();
-            if(!telaCriada) {
-                ponteiro = gerarPosições();
-                telaCriada = true;
-            }
-            gerarFiguras(canvas, paint, ponteiro);
+    private ArrayList<Point> posicoes = new ArrayList<>();
 
-    }
-
-
-
-    public ArrayList<Point> gerarPosições(){
+    public ArrayList<Point> gerarPosicoes(){
         ArrayList<Point> Ponteiros = new ArrayList<>();
         int x = getWidth();
         int y = getHeight();
-        Point posição1 = new Point(x/4,2*y/4);
-        Point posição2 = new Point(x/4,3*y/4);
-        Point posição3 = new Point(3*x/4,2*y/4);
-        Point posição4 = new Point(3*x/4,3*y/4);
+        Point posição1 = new Point(3*x/6,2*y/6);
+        Point posição2 = new Point(2*x/6,3*y/6);
+        Point posição3 = new Point(3*x/6,4*y/6);
+        Point posição4 = new Point(4*x/6,3*y/6);
         Ponteiros.add(posição1);
         Ponteiros.add(posição2);
         Ponteiros.add(posição3);
         Ponteiros.add(posição4);
 
-        Collections.shuffle(Ponteiros);
 
         return Ponteiros;
 
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        posicoes = gerarPosicoes();
+        gerarFiguras(canvas, paint, posicoes);
+
+    }
     public void gerarFiguras(Canvas canvas, Paint paint,ArrayList<Point> Ponteiros) {
 
-        Path path = new Path();
-        int x = getWidth();
         int y = getHeight();
-        int raio=100;
+        float raio= (float) (1.3*y/6);
         Random random = new Random ();
         int a = random.nextInt(156)+100;
         int r = random.nextInt(156)+100;
@@ -77,63 +70,13 @@ public class FigurasView extends View {
         paint.setColor(Color.argb(a, r, g, b));
         //Circulo
         canvas.drawCircle(Ponteiros.get(0).x,Ponteiros.get(0).y,raio,paint);
-
-        //Quadrado
-        canvas.drawRect((Ponteiros.get(1).x)-raio,(Ponteiros.get(1).y)-raio,raio+Ponteiros.get(1).x,raio+Ponteiros.get(1).y,paint);
-
-        //Triangulo
-        // canvas.drawCircle(3*x/4,2*y/4,raio,paint);
-        Point a1 = new Point(Ponteiros.get(2).x, (Ponteiros.get(2).y)-raio);
-        Point b2 = new Point(raio+Ponteiros.get(2).x, raio+Ponteiros.get(2).y);
-        Point c3 = new Point((Ponteiros.get(2).x)-raio, raio+Ponteiros.get(2).y);
-
-//        path.setFillType(Path.FillType.EVEN_ODD);
-
-        path.lineTo(a1.x, a1.y);
-        path.lineTo(b2.x, b2.y);
-        path.lineTo(c3.x, c3.y);
-        path.lineTo(a1.x, a1.y);
-        path.close();
-
-        canvas.drawPath(path, paint);
-        //Retangulo
-
-        canvas.drawRect((Ponteiros.get(3).x)-raio/2,(Ponteiros.get(3).y)-raio,raio/2+Ponteiros.get(3).x,raio+Ponteiros.get(3).y,paint);
+        //Circulo
+        canvas.drawCircle(Ponteiros.get(1).x,Ponteiros.get(1).y,raio,paint);
+        //Circulo
+        canvas.drawCircle(Ponteiros.get(2).x,Ponteiros.get(2).y,raio,paint);
+        //Circulo
+        canvas.drawCircle(Ponteiros.get(3).x,Ponteiros.get(3).y,raio,paint);
 
 
     }
-    @Override
-    public boolean onTouchEvent(final MotionEvent event ) {
-        Canvas canvas = new Canvas();
-
-
-
-        final float x = event.getX();
-        final float y = event.getY();
-
-        switch (event.getAction()) {
-
-            case MotionEvent.ACTION_DOWN:
-                Log.i("ON TOUCH EVENT", "DOWN + X: " + x + " Y " + y);
-
-                if ((y <= ponteiro.get(0).y+100 && x<=ponteiro.get(0).x+100)&&(y <= ponteiro.get(0).y-100 && x<=ponteiro.get(0).x-100)){
-                    telaCriada=false;
-                    draw(canvas);
-                    postInvalidate();
-
-
-
-                }
-
-                break;
-
-            default:
-                break;
-        }
-
-        return super.onTouchEvent(event);
-
-
-    }
-
 }
